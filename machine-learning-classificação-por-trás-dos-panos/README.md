14638187ee486c16cd19d3e9143ad221987b8c5f
gustavocastrow <gustavocastrocs@gmail.com>
2021-12-26 20:17:19 -0300

    Módulo 02

--- machine-learning-classificação-por-trás-dos-panos/README.md
+++ machine-learning-classificação-por-trás-dos-panos/README.md
@@ -10,3 +10,13 @@
 - [x] 1.6 Exemplo do mundo web.
 - [x] 1.7 Classificação e usuário.
 
+*Módulo 2:Importando, classificando e validando um modelo*
+
+- [x] 2.1 Lendo o arquivo CSV.
+- [x] 2.2 Utilizando o algoritmo MultinomialNB.
+- [x] 2.3 Obtendo taxa de acerto.
+- [x] 2.4 Treinando e testando o algoritmo com metade dos dados.
+- [x] 2.5 Resumo.
+- [x] 2.6 Revisando o código.
+
+
--- machine-learning-classificação-por-trás-dos-panos/classificacao.py
+++ machine-learning-classificação-por-trás-dos-panos/classificacao.py
@@ -1,64 +1,40 @@
-
-from sklearn.naive_bayes import MultinomialNB
-modelo = MultinomialNB()
-
-porco1 = [1, 1, 0]
-porco2 = [1, 1, 0]
-porco3 = [1, 1, 0]
+# eh gordinho? tem perninha curta? se faz au au
+porco1 =    [1, 1, 0]
+porco2 =    [1, 1, 0]
+porco3 =    [1, 1, 0]
 cachorro1 = [1, 1, 1]
 cachorro2 = [0, 1, 1]
 cachorro3 = [0, 1, 1]
 
 dados = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
-#gordinho/perna curta/auau
-# 1: PORCO
-# -1: CACHORRO 
-
 marcacoes = [1, 1, 1, -1, -1, -1]
 
-misterioso1 = [1, 1, 1] #CACHORRO
-misterioso2 = [1, 0, 0] #PORCO
-misterioso3 = [0, 0, 1] #CACHORRO
+from sklearn.naive_bayes import MultinomialNB
 
+modelo = MultinomialNB()
+modelo.fit(dados, marcacoes)
+
+misterioso1 = [1, 1, 1]
+misterioso2 = [1, 0, 0]
+misterioso3 = [1, 0, 1]
 testes = [misterioso1, misterioso2, misterioso3]
+marcacoes_teste = [-1, 1, 1]
 
-modelo.fit(dados, marcacoes) #Adequando o modelo aos meus dados
 resultado = modelo.predict(testes)
-
 print(resultado)
 
-#Criando uma marcamção de teste para os elementos misteriosos utilziando o array "marcacoes_teste" indicando o que é
-#cada elemento misterioso
-
-marcacoes_teste = [-1, 1, -1] #CACHORRO, PORCO, CACHORRO
-
-#Calculando a diferença entre as variaveis "resultado" e as "marcacoes_teste" e atribuindo uma variavel
-#chamada diferenças
 diferencas = resultado - marcacoes_teste
-print(f"Diferenças: {diferencas}")
-
-#Percorrendo o array "diferenças" e devolvendo todos os seus elementos que forem iguais a 0 e atribuindo
-#a uma variavel chamada "acertos"
 
 acertos = [d for d in diferencas if d==0]
-print(f"Acertos: {acertos}")
-
-#Calculando o total de acertos pegando o tamanho do array "acertos" com o método len() e atribuindo
-#uma variável "total_de_acertos"
 total_de_acertos = len(acertos)
-print(f"Total de acertos: {total_de_acertos}")
+total_de_elementos = len(testes)
 
-#Calculando o total de elementos pegando o tamanho do array "teste" com o método len() e atribuindo
-#uma variável "total_de_elementos"
-total_elementos = len(testes)
-print(f"Total de elementos: {total_elementos}")
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+print(taxa_de_acerto)
 
-#Calculando a taxa de acerto multiplicando por 100.0 pela divisão das variáveis "total_de_acertos"
-#e "total_de_elementos" e atribuindo a variavel "taxa_de_acerto"
 
-taxa_de_acertos = 100.0 * total_de_acertos / total_elementos
-print(f"Taxa de acerto: {taxa_de_acertos}")
 
-print(f"Resultado: {resultado}")
-print(f"Diferenças: {diferencas}")
-print(f"Taxa de Acerto: {taxa_de_acertos}")
+
+
+
+
